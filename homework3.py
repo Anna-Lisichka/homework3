801729ee7584bbcd685901a419efed831e2ccfc0
Anna-Lisichka <an_na22@mail.ru>
2024-05-21 21:28:58 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Anna'
+print(name)
+age = 25
+print(age)
+New_age = age + 5  # будем считать, что это максимальный возраст для студентов
+print(New_age)
+is_student = age < New_age
+print(is_student)
